dfc7097f28ef62a796f2f34c6b5d68bd5c093bb0
2190154 <yongdo.kim@hanwha.com>
2023-02-15 16:52:42 +0900

    4번째 과제 완료

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import GlobalLayout from './pages/_layout'
 const DynamicIndex = React.lazy(() => import('./pages/index'));
 const DynamicStudy2Index = React.lazy(() => import('./pages/study2/index'));
 const DynamicStudy3Index = React.lazy(() => import('./pages/study3/index'));
+const DynamicStudy4Index = React.lazy(() => import('./pages/study4/index'));
 const DynamicTempIndex = React.lazy(() => import('./pages/temp/index'));
 
 
@@ -16,6 +17,7 @@ export const routes = [
       { path: '/', element: <DynamicIndex />, index: true},
       { path: '/study2', element: <DynamicStudy2Index />, index: true},
       { path: '/study3', element: <DynamicStudy3Index />, index: true},
+      { path: '/study4', element: <DynamicStudy4Index />, index: true},
       { path: '/temp', element: <DynamicTempIndex />, index: true},
     ]
   }
@@ -25,5 +27,6 @@ export const pages = [
   { route: '/' },
   { route: '/study2' },
   { route: '/study3' },
+  { route: '/study4' },
   { route: '/temp' },
 ]
